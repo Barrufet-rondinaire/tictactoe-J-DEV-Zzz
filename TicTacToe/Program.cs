0eca21350d2f7403a72a6e7429bb593f5ca0b629
‘Joel’ <jcabrera@cendrassos.net>
2025-03-27 19:52:04 +0100

    ..

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace TicTacToe;
+﻿using System.Net.Http.Json;
+namespace TicTacToe;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main()
     {
-        Console.WriteLine("Hello, World!");
+       using var client = new HttpClient();
+       {
+           var r = await client.GetFromJsonAsync <List<string>> (
+               "http://localhost:8080/jugadors"
+           );
+
+           foreach (var comentari in r)
+           {
+               
+           }
+           {
+               
+           }
+       }
     }
 }
